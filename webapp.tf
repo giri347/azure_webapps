ac853192abee6e365e8b8a6ac6ba2d311c1939e6
Giribabu Chitteni <Giribabu.Chitteni@einfochips.com>
2023-01-19 09:51:37 +0530

    added webapps

--- webapp.tf
+++ webapp.tf
@@ -1,5 +1,4 @@
 
-/*
 resource "azurerm_app_service_plan" "einfoplan" {
   name                = "einfoplan-appserviceplan"
   location            = azurerm_resource_group.appgrp.location
@@ -12,13 +11,13 @@ resource "azurerm_app_service_plan" "einfoplan" {
 }
 
 #resource "azurerm_app_service" "einfowebapp" {
-  #count               = 2
-  #name                = "einfo-webapp${count.index + 1}"
-  #location            = azurerm_resource_group.appgrp.location
-  ##resource_group_name = azurerm_resource_group.appgrp.name
-  #app_service_plan_id = azurerm_app_service_plan.einfoplan.id
-  resource "azurerm_app_service" "einfowebapp" {
-    for_each = toset(["einfodevwebapp" , "einfoprowebapp"])
+#count               = 2
+#name                = "einfo-webapp${count.index + 1}"
+#location            = azurerm_resource_group.appgrp.location
+##resource_group_name = azurerm_resource_group.appgrp.name
+#app_service_plan_id = azurerm_app_service_plan.einfoplan.id
+resource "azurerm_app_service" "einfowebapp" {
+  for_each            = toset(["einfodevwebapp", "einfoprowebapp"])
   name                = each.key
   location            = azurerm_resource_group.appgrp.location
   resource_group_name = azurerm_resource_group.appgrp.name
@@ -40,4 +39,3 @@ resource "azurerm_app_service_plan" "einfoplan" {
     value = "Server=some-server.mydomain.com;Integrated Security=SSPI"
   }
 }
-*/
